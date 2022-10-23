55f4383c6a6e824616943af68a42ea9b00a4dabb
Rob Challen <rob@terminological.co.uk>
2022-10-23 21:25:53 +0100

    0.0.0.9000 initial commit

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c(".grp_id",".rows","count","fill","group","negative",
+"positive","time","timeseries","total.grp_id",".rows",":=","count","fill",
+"group","negative","positive","time","timeseries","total"))
---
+++ inst/examples/time-period-example.R
@@ -0,0 +1,15 @@
+# 100 weeks from 2020-01-01
+
+tmp = as.time_period(0:100, 7, "2020-01-01")
+as.Date(tmp)
+
+tmp2 = as.integer(as.Date(tmp))
+# testthat::expect_true(all(na.omit(tmp2-lag(tmp2)) == 7))
+
+tmp2 = as.time_period(0:23, 1/24, "2020-01-01")
+as.POSIXct(tmp2)
+
+# convert timeseries to new "unit"
+tmp = as.time_period(0:100, 7, "2020-01-01")
+tmp2 = as.time_period(tmp,1)
+testthat::expect_equal(as.numeric(tmp2), 0:100*7)
